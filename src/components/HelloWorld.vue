32edb8d09cb6d2ee12cc40a4b686477daff5afaa
Mani Movassagh <mani.mgh@gmail.com>
2022-07-08 23:47:57 +0200

    Experience new suspence component from vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script async setup>
+import { ref } from "vue";
+import axios from "axios";
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+// const posts = await res.json()
+
+const data=await axios.get('https://jsonplaceholder.typicode.com/todos')
+// console.log(data.data);
+</script>
+
+<template>
+  <div v-for="t in data.data" :key="t.id">{{ t.title }}</div>
+</template>
+
+<style scoped></style>
