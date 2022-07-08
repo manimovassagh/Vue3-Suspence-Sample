750cc6d460ed43f9527117333975bd4210618146
Mani Movassagh <mani.mgh@gmail.com>
2022-07-08 23:50:01 +0200

    add some docs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script async setup>
 import { ref } from "vue";
 import axios from "axios";
-
+//It is possible to use fetch and axios to fetch data from the server.
 // const res = await fetch('https://jsonplaceholder.typicode.com/todos')
 // const posts = await res.json()
 
